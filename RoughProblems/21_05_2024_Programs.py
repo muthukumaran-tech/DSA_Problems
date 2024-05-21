4c74ba983af56f2c0b9f0a0f586191750aab38da
unknown <muthukumaran.p@cubesimple.com>
2024-05-21 22:19:31 +0530

    swap values different approaches

--- RoughProblems/21_05_2024_Programs.py
+++ RoughProblems/21_05_2024_Programs.py
@@ -1,7 +1,24 @@
+
+#using temp variable
 def swap(a,b):
     temp = a
     a = b
     b = temp
+    return a,b
+
+
+#using comma variable
+
+def swap(a,b):
+    a,b = b,a
+    return a,b
+
+# using without temp
+
+def swap(a,b):
+    a = a+b
+    b = a-b
+    a = a-b
     return a,b
 
 print(swap(10,50))
